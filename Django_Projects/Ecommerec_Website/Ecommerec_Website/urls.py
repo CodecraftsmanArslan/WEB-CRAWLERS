1ac8b0a49ae837f7eac07cbc3062e7afa8dca6fe
This PC <arslanaziz2017@gmail.com>
2024-05-03 17:33:38 +0500

    changes in file

--- Django_Projects/Ecommerec_Website/Ecommerec_Website/urls.py
+++ Django_Projects/Ecommerec_Website/Ecommerec_Website/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns=[
     path('',views.user_login,name='login'),
     path('registration',views.registration_form,name='registration_form'),
     path('item',views.add_item,name='item'),
-
 ]
